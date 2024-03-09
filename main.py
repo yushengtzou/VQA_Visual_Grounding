4b398a56f6d333770f7c991404c9c2deeaf112a9
Yu-Sheng Tzou <daniel.tzou1021@gmail.com>
2024-03-09 22:28:05 +0800

    add main.py and resizeImage.py

---
+++ main.py
@@ -0,0 +1,43 @@
+'''  ==========================================================================
+ *
+ *       Filename:  main.py
+ *
+ *    Description:  此項目的主程式  
+ *
+ *        Version:  1.0
+ *        Created:  2024/03/09
+ *       Revision:  none
+ *       Compiler:  
+ *
+ *         Author:  鄒雨笙 
+ *   Organization:  
+ *
+ * ==========================================================================
+'''
+
+
+# 引入相關副程式
+# import A
+# import B
+# import C
+# import D
+# import E
+
+
+if __name__ == '__main__':
+    # 呼叫 A 副程式
+    print('Start Coding the Real Thing')
+    print('I love Coding')
+    # 呼叫 B 副程式
+    # cool_func()
+    # 呼叫 C 副程式
+    # cool_func()
+    # 呼叫 D 副程式
+    # cool_func()
+    # 呼叫 E 副程式
+    # cool_func()
+
+
+
+
+
---
+++ resizeImage.py
@@ -0,0 +1,45 @@
+'''  ==========================================================================
+ *
+ *       Filename:  resizeImage.py
+ *
+ *    Description:  Resize the images to 224x224  
+ *
+ *        Version:  1.0
+ *        Created:  2024/03/09
+ *       Revision:  none
+ *       Compiler:  
+ *
+ *         Author:  鄒雨笙 
+ *   Organization:  
+ *
+ * ==========================================================================
+'''
+
+
+# 引入相關副程式
+# import A
+# import B
+# import C
+# import D
+# import E
+
+def cropImage()
+
+
+if __name__ == '__main__':
+    # 呼叫 A 副程式
+    print('Start Coding the Real Thing')
+    print('I love Coding')
+    # 呼叫 B 副程式
+    # cool_func()
+    # 呼叫 C 副程式
+    # cool_func()
+    # 呼叫 D 副程式
+    # cool_func()
+    # 呼叫 E 副程式
+    # cool_func()
+
+
+
+
+
